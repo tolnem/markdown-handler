7cb84c5284432ff356f92e0ce4087f1193e32c68
David Tolnem <david@xstream.dk>
2012-01-16 16:43:19 +0100

    Add a table of contents heading, and make the toc expand and close on click

--- markdown/markdown.js
+++ markdown/markdown.js
@@ -1,19 +1,33 @@
 /* CREATE TOC */
 
 function createTOC() {
-	var toc = $("<ul></ul>").attr("id", "innertoc");
-	var toBeTOCced = $(":header").each(function (index) {
-		if ($(this).prop("tagName") != "H1") {
-			var headerId = "header-" + index;
-			$(this).before($("<a name=\"" + headerId + "\">"));
-			toc.append($("<li class=\"" + $(this).prop("tagName") + "\"><a href=\"#" + headerId + "\">" + $(this).html() + "</a></li>"));
+	var headers = $("h2, h3, h4, h5"),
+		toc = $("<ul id='innertoc' style='display: none;'></ul>"),
+		wrapper = $("<div id='toc'><h2>Table of contents <span>&#8675;</span><span style='display: none'>&#8673;</span></h2></div>").append(toc);
+		wrapper.find('h2').click(function() {
+			$('#innertoc').toggle();
+			$('#toc h2 span').toggle();
+		});
+	if (!headers.length) {
+		return false;
+	}
+	headers.each(function (index) {
+		var $elem = $(this),
+			headerId = $elem.attr('id');
+		if (!headerId) {
+			headerId = "header-" + index;;
+			$elem.before($("<a name=\"" + headerId + "\">"));
 		}
+		toc.append($("<li class=\"" + $elem.prop("tagName") + "\"><a href=\"#" + headerId + "\">" + $elem.html() + "</a></li>"));
 	});
-	return toc;
+	
+	return wrapper;
 }
 
-
 $(function () {
-	$("body").children().first().after(createTOC());
+	var toc = createTOC();
+	if (toc) {
+		$("body").children().first().after(toc);
+	}
 });
 
